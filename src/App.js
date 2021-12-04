8f61ebb3b4e692a0d963166e8d768116d1811ee4
望月祐汰 <mczk9402@gmail.com>
2021-12-04 10:04:35 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = (props) => {
   const [state, setState] = useState(props);
   const { name, price } = state;
+
+  useEffect(() => {
+    console.log("[]無し");
+  });
+
+  useEffect(() => {
+    console.log("[]");
+  }, []);
+
+  useEffect(() => {
+    console.log("[state.name]");
+  }, [state.name]);
 
   return (
     <div>
       <p>
         現在の{name}は{price}円です
       </p>
+      {/* useEffectはレンダリングの後に実行される */}
       <button onClick={() => setState({ ...state, price: price + 1 })}>+1</button>
       <button onClick={() => setState({ ...state, price: price - 1 })}>-1</button>
       <button onClick={() => setState(props)}>Reset</button>
